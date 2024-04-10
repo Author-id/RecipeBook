471010293dc2cd4adbb3ad9ae76e14f04940cfa2
Asus <semyanovandrew@yandex.ru>
2024-04-10 18:59:16 +0300

    add __all__ to apps

--- recipebook/feedback/apps.py
+++ recipebook/feedback/apps.py
@@ -6,3 +6,6 @@ class FeedbackConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "feedback"
     verbose_name = gettext_lazy("app__feedback")
+
+
+__all__ = []
--- recipebook/users/apps.py
+++ recipebook/users/apps.py
@@ -6,3 +6,6 @@ class UsersConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "users"
     verbose_name = gettext_lazy("app__users")
+
+
+__all__ = []
